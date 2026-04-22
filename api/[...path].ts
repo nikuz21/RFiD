9e84fb06f60a7262c4acbfcdbe756d385687a528
Nikuz Baito <nikuzbaito646@gmail.com>
2026-04-22 15:02:15 +0800

    fix vercel deployment config

--- api/[...path].ts
+++ api/[...path].ts
@@ -4,3 +4,9 @@ import app from "../artifacts/api-server/src/app";
 export default function handler(req: IncomingMessage, res: ServerResponse) {
   return app(req, res);
 }
+import type { IncomingMessage, ServerResponse } from "node:http";
+import app from "../artifacts/api-server/src/app";
+
+export default function handler(req: IncomingMessage, res: ServerResponse) {
+  return app(req, res);
+}
