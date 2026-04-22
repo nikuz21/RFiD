9d6a8e69e37f263e5ac0a0e2b63177e2847baf4a
Nikuz Baito <nikuzbaito646@gmail.com>
2026-04-22 15:18:10 +0800

    update files for deployment fix

--- api/[...path].ts
+++ api/[...path].ts
@@ -4,15 +4,3 @@ import app from "../artifacts/api-server/src/app";
 export default function handler(req: IncomingMessage, res: ServerResponse) {
   return app(req, res);
 }
-import type { IncomingMessage, ServerResponse } from "node:http";
-import app from "../artifacts/api-server/src/app";
-
-export default function handler(req: IncomingMessage, res: ServerResponse) {
-  return app(req, res);
-}
-import type { IncomingMessage, ServerResponse } from "node:http";
-import app from "../artifacts/api-server/src/app";
-
-export default function handler(req: IncomingMessage, res: ServerResponse) {
-  return app(req, res);
-}
